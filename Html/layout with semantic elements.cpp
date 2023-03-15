f3f3312acf83901f940b669d88aede50ae99b2f6
Abdeltawab Wagieh <98906055+abdeltawab10@users.noreply.github.com>
2023-03-15 17:54:26 +0200

    Create layout with semantic elements.cpp

---
+++ Html/layout with semantic elements.cpp
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- render will be a  standard mode already 
+            supports a lot of HTML5 and CSS3 but not the 
+            full range of the specifications -->
+        <meta name="description " content="this our book store"/>
+        <title>
+            Book Store
+        </title>
+        <meta charset="UTF-8"/>
+        <style></style>
+        <script></script>
+        <link rel="" href="">
+    </head>
+    <bod>
+        <h1> Book Store</h1>
+        <p>Welcome> to my page book store</p>
+        <header>
+            <h2>Logo</h2>
+            <ul>
+                <li>Home</li>
+                <li>about</li>
+                <li>services</li>
+                <li>contact us</li>
+            </ul>
+        </header>
+        <hr/>
+        <!--nav for one time only -->
+        <nav>
+            <ul>
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+            </ul>
+
+        </nav>
+        <section>
+            <!-- figure is a div of a section -->
+            <figure>
+            <img src="imags/earth-g4c9327dfa_1920.jpg">
+            <figcaption>caption of the image</figcaption>
+            </figure>
+        </section>
+        <aside>
+            sideber
+        </aside>
+        <hr/>
+        <footer>
+            footer
+        </footer>
+
+    </body>
+</html>
